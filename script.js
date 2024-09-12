1eda554f89baf5af64b3ddbd08bb50bd91587573
Maksim Shubny <shubny.maksim@yandex.ru>
2024-09-12 18:04:59 +0500

    Добавил переменные, alert и console.log в script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
+
+alert("Привет, куратор, давай дружить :)");
+console.log("Как дела?");
