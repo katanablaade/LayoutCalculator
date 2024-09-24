23cb9905306619677c4b6e3dff5711252218277d
Maksim Shubny <shubny.maksim@yandex.ru>
2024-09-24 19:08:32 +0500

    Выполнил lesson08

--- script.js
+++ script.js
@@ -63,16 +63,16 @@ const appData = {
         price = prompt('Сколько это будет стоить?');
       } while (!appData.isNumber(price));
 
-      appData.services[name] = +price;
+      appData.services[name + i] = +price;
     }
 
     appData.adaptive = confirm('Нужен ли адаптив на сайте?');
   },
 
   addPrices: function () {
-    for (let screen of appData.screens) {
-      appData.screenPrice += +screen.price;
-    }
+    appData.screenPrice = appData.screens.reduce(function (acc, item) {
+      return acc + Number(item.price);
+    }, 0);
 
     for (let key in appData.services) {
       appData.allServicePrices += appData.services[key];
