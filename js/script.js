a39514c517b06d256dd0316a59ef7759dfd3e807
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-02 17:40:48 +0500

    Рефакторинг lesson14

--- js/script.js
+++ js/script.js
@@ -26,12 +26,16 @@ const cmsVariants = document.querySelector('.hidden-cms-variants');
 const otherInput = document.querySelector(
   '.hidden-cms-variants .main-controls__input'
 );
+const cmsOtherInput = document.querySelector('#cms-other-input');
+
 const optionOther = document.querySelector(
   '.hidden-cms-variants #cms-select option[value = other]'
 );
 const optionWordPress = document.querySelector(
   '.hidden-cms-variants #cms-select option[value = "50"]'
 );
+const customCheckBox = document.querySelectorAll('.custom-checkbox');
+
 const cmsSelect = document.querySelector('.hidden-cms-variants #cms-select ');
 let screenSearch = document.querySelectorAll('.screen');
 
@@ -49,6 +53,24 @@ const appData = {
   servicesNumber: {},
   countScreens: 0,
   wordPress: 1,
+  other: 0,
+
+  init: function () {
+    this.addTitle();
+    handlerBtnStart.addEventListener('click', () => {
+      this.checkField();
+    });
+    handlerBtnReset.addEventListener('click', this.reset.bind(appData));
+    plusBtn.addEventListener('click', this.addScreenBlock);
+    inputRange.addEventListener('input', this.getRollback.bind(appData));
+    cmsCheckBox.addEventListener('change', this.cmsOpen.bind(appData));
+    cmsVariants.addEventListener('change', this.otherOpen.bind(appData));
+  },
+
+  reset: function () {
+    appData.removeScreens();
+    appData.removeValue();
+  },
 
   checkField: function () {
     screenSearch = document.querySelectorAll('.screen');
@@ -76,30 +98,25 @@ const appData = {
     }
   },
 
-  reset: function () {
-    const select = document.querySelector('select');
-    const input = document.querySelector('input');
+  removeScreens: function () {
     const totalInput = document.querySelectorAll('.total-input');
-
-    handlerBtnStart.style.display = 'block';
-    handlerBtnReset.style.display = 'none';
-    cmsSelect.selectedIndex = 0;
-    cmsCheckBox.checked = false;
-    cmsVariants.style.display = 'none';
-    otherInput.style.display = 'none';
-    this.wordPress = 1;
-
     screenSearch.forEach((item, index) => {
       if (index !== 0) {
         item.remove();
       }
-      console.log(screenSearch);
     });
 
     totalInput.forEach((item) => {
       item.value = 0;
     });
+  },
 
+  removeValue: function () {
+    const select = document.querySelector('select');
+    const input = document.querySelector('input');
+
+    this.other = 0;
+    this.wordPress = 1;
     this.title = '';
     this.screens = [];
     this.adaptive = true;
@@ -112,11 +129,21 @@ const appData = {
     this.servicePricesPercent = 0;
     this.servicePricesNumber = 0;
     this.countScreens = 0;
-    spanRange.textContent = 0 + '%';
-    inputRange.value = 0;
 
+    handlerBtnStart.style.display = 'block';
+    handlerBtnReset.style.display = 'none';
+    cmsVariants.style.display = 'none';
+    otherInput.style.display = 'none';
     input.value = '';
     select.value = '';
+    cmsOtherInput.value = '';
+
+    customCheckBox.forEach((item) => {
+      item.checked = false;
+    });
+    spanRange.textContent = 0 + '%';
+    cmsSelect.selectedIndex = 0;
+    inputRange.value = 0;
     input.disabled = false;
     select.disabled = false;
   },
@@ -134,25 +161,16 @@ const appData = {
     if (optionOther.selected) {
       otherInput.style.display = 'block';
       this.wordPress = 1;
+      this.other = this.other + +cmsOtherInput.value;
     } else if (optionWordPress.selected) {
       otherInput.style.display = 'none';
       this.wordPress = 1.5;
+      this.other = 0;
     } else {
       otherInput.style.display = 'none';
       this.wordPress = 1;
+      this.other = 0;
     }
-  },
-
-  init: function () {
-    appData.addTitle();
-    handlerBtnStart.addEventListener('click', () => {
-      this.checkField();
-    });
-    handlerBtnReset.addEventListener('click', this.reset.bind(appData));
-    plusBtn.addEventListener('click', this.addScreenBlock);
-    inputRange.addEventListener('input', this.getRollback.bind(appData));
-    cmsCheckBox.addEventListener('change', this.cmsOpen.bind(appData));
-    cmsVariants.addEventListener('change', this.otherOpen.bind(appData));
   },
 
   getRollback: function (event) {
@@ -163,7 +181,6 @@ const appData = {
       this.servicePercentPrice = Math.ceil(
         this.fullPrice - this.fullPrice * (this.rollback / 100)
       );
-      console.log(appData.wordPress);
       totalCountRollback.value = this.servicePercentPrice;
     }
   },
@@ -236,7 +253,6 @@ const appData = {
   addScreenBlock: function () {
     screenSearch = document.querySelectorAll('.screen');
     let cloneScreen = screenSearch[0].cloneNode(true);
-    console.log(screenSearch);
     screenSearch[screenSearch.length - 1].after(cloneScreen);
   },
 
@@ -253,11 +269,13 @@ const appData = {
       this.servicePricesPercent +=
         this.screenPrice * (this.servicesPercent[key] / 100);
     }
-    this.fullPrice =
+    this.fullPrice = Math.ceil(
       (+this.screenPrice +
         this.servicePricesPercent +
         this.servicePricesNumber) *
-      appData.wordPress;
+        (1 + this.other / 100) *
+        this.wordPress
+    );
     this.servicePercentPrice =
       this.fullPrice - this.fullPrice * (this.rollback / 100);
 
